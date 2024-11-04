df080fd392d325b141d5d8cbc82bd687aa811235
priyanka5555 <75875238+priyanka5555@users.noreply.github.com>
2024-11-04 18:12:11 -0500

    Update admin.py

--- routers/admin.py
+++ routers/admin.py
@@ -3,9 +3,9 @@ from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException, Path
 from starlette import status
-from ..database import SessionLocal
-from ..models import Todos
-from .auth import get_current_user
+from database import SessionLocal
+from models import Todos
+from auth import get_current_user
 
 router = APIRouter(
     prefix='/admin',
